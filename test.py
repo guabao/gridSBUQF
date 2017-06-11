fc5a0714f90cbda5e726d6822af1afad82942476
guabao <wjz270025872@gmail.com>
2017-06-11 18:24:38 -0400

    Create test.py
    change gridOneField to gridDense

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ def tutorial():
 
     mx = numpy.random.rand(10, 4)
     dte = pandas.date_range('20130101', periods = 10)
-    g = grid.gridOneField(mx, dte, list('ABCD'), 'ask')
+    g = grid.gridDense(mx, dte, list('ABCD'), 'ask')
     mx1 = numpy.random.rand(10, 4)
     g.append(mx1, field = 'bid')
     return g
