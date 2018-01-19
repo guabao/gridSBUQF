a8ed8f9db25864929d018f079c89106c4db67067
wjz270025872@gmail.com <guabao>
2018-01-19 15:35:08 -0500

    fix cache bug

--- util/cache.py
+++ util/cache.py
@@ -1,5 +1,8 @@
 __doc__ = '''cache module'''
 
+from functools import wraps
+import copy
+import inspect
 import sys
 
 import numpy
@@ -10,21 +13,23 @@ GLOBALS_HASHS = numpy.zeros(GLOBALS_NUM+1)
 
 
 def cacheMemory(func):
+    @wraps(func)
     def func_wrapper(*args):
         global_val = sys.modules[func.__module__].cache.GLOBALS_VALS
         global_num = sys.modules[func.__module__].cache.GLOBALS_NUM
         global_hash = sys.modules[func.__module__].cache.GLOBALS_HASHS
-        func_hash = func.__hash__()
+        func_hash = hash(inspect.getsource(func))
+        #func_hash = func.__hash__()
         flag = global_hash == func_hash
         if numpy.any(flag):
-            return global_val[numpy.argwhere(flag).ravel() - 1]
+            return global_val[int(numpy.argwhere(flag).ravel()) - 1]
         else:
             result = func(*args)
             if len(global_val) == global_num:
                 temp = global_val[1:]
                 global_val = temp
                 global_hash[:-1] = global_hash[1:]
-            global_val += [result]
+            global_val += [copy.deepcopy(result)]
             global_hash[len(global_val)] = func_hash
             return result
     return func_wrapper
